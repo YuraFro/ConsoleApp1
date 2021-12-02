a18312da5e7a1a095634745d2018d514ad0c4efe
Юрий Фролов <urafrolov0101@gmail.com>
2021-12-02 15:32:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/MyArray.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //Класс для создания и работы с массивом
+    public class MyArray
+    {
+        //Создаваемый рандомный массив
+        private int[] array1;
+        //Список со значениями без пары
+        private List<int> listNoPar;
+        //Список с парными значениями
+        private List<int> listPar;
+
+        //Конструктор с входными параметрами для настройки массива
+        public MyArray (int n, int min, int max)
+        {
+            //Создание объекта
+            array1 = new int[n];
+            Random r = new Random();
+
+            //Заполнение массива ранломными значениями в установленном диапозоне и размером
+            for (int i = 0; i < n; i++)
+            {
+                array1[i] = r.Next(min, max);
+            }
+        }
+
+        //Возращение массива
+        public IEnumerable<int> GetArray ()
+        {
+            return array1;
+        }
+
+        //Выборка не парных элементов массива
+        public IEnumerable<int> SelectNoPar ()
+        {
+            //Создание объектов
+            listPar = new List<int> ();
+            listNoPar = new List<int> ();
+
+            //Сравнение элемента массива со всеми идущими после него
+            for (int i = 0; i < array1.Length - 1; i++)
+            {
+                for (int j = i+1; j <= array1.Length - 1; j++)
+                {
+                    //Если значение элемента массива совпадает с последующим, то он заносится в отдельный список
+                    if (array1[i] == array1[j])
+                        listPar.Add(array1[i]);
+                }
+            }
+
+            //Перебор элементов массива
+            foreach (int number1 in array1)
+            {
+                //Если элемент массива не содержится в списке с парными элементами, то он заносится в отдельный список
+                if (!listPar.Contains(number1))
+                    listNoPar.Add(number1);
+            }
+
+            return listNoPar;
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Добавил таймер для вывода каждые 4 секнды новых массивов
+
+            Timer MyTimer = new Timer();
+            MyTimer.Interval = 4000;
+            MyTimer.Elapsed += MyTimer_Elapsed;
+            MyTimer.Start();
+
+            Console.ReadLine();
+
+            MyTimer.Stop();
+
+            Console.WriteLine();
+
+        }
+
+
+        private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+            => WorkArray();
+
+
+        //Метод, в котором будет осуществляться создание массива и поиск непарных элементов
+        public static void WorkArray()
+        {
+            //Создание объекта класса и установление параметров для массива
+            MyArray array = new MyArray(10, 0, 10);
+
+            //Вывод элементов созданного изначально массива
+            Console.WriteLine(OutMass(array.GetArray()));
+            //Вывод не парных элементов массива
+            Console.WriteLine(OutMass(array.SelectNoPar()));
+
+
+            Console.ReadLine();
+        }
+
+        //Метод для вывода элементов массива в строку
+        public static string OutMass(IEnumerable<int> array)
+        {
+            string stringarray = "";
+            foreach (int x in array)
+                stringarray = stringarray + x + " ";
+            return stringarray;
+        }
+    }
+
+
+}
